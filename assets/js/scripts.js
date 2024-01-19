fbaf88f28d83313013820fc2594b7085975d598a
Mann Jadwani <mannjadwani2004@gmail.com>
2024-01-19 14:52:51 +0530

    3d

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/MsWNe0ppYWRXNDC0/scene.splinecode');
