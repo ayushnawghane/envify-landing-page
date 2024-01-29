756af7b8246d4d6286fca5f51f9d132354e4096d
Ayush <ayushnawghane2003@gmail.com>
2024-01-29 10:32:42 +0530

    mobile nav done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,40 @@ $(window).mousemove(function (e) {
 		`translateX(calc(${e.clientX}px - 1.25rem)) translateY(calc(${e.clientY}px - 1.25rem))`
 	);
 });
+
+
+
+// navbar mobile
+
+(function($) { 
+	$(function() { 
+  
+	  //  open and close nav 
+	  $('#navbar-toggle').click(function() {
+		$('nav ul').slideToggle();
+	  });
+  
+  
+	  // Hamburger toggle
+	  $('#navbar-toggle').on('click', function() {
+		this.classList.toggle('active');
+	  });
+  
+  
+	  // If a link has a dropdown, add sub menu toggle.
+	  $('nav ul li a:not(:only-child)').click(function(e) {
+		$(this).siblings('.navbar-dropdown').slideToggle("slow");
+  
+		// Close dropdown when select another dropdown
+		$('.navbar-dropdown').not($(this).siblings()).hide("slow");
+		e.stopPropagation();
+	  });
+  
+  
+	  // Click outside the dropdown will remove the dropdown class
+	  $('html').click(function() {
+		$('.navbar-dropdown').hide();
+	  });
+	}); 
+  })(jQuery); 
+  
